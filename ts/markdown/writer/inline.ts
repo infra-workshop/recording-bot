68d78138da96de97882178c880f9d2159bf77fae
anatawa12 <anatawa12@icloud.com>
2019-08-04 22:59:05 +0900

    add escapeHtml

--- ts/markdown/writer/inline.ts
+++ ts/markdown/writer/inline.ts
@@ -1,12 +1,10 @@
 import {CustomWriter} from "./CustomWriter";
 import {InlineToken} from "../Tokens";
 import {Writer} from "./Writer";
+import {escapeHtml} from "../util";
 
 const inline: CustomWriter<InlineToken> = function inline(writer: Writer, token: InlineToken) {
-    writer.append(token.content.replace('&', "&amp;")
-        .replace('<', "&lt;")
-        .replace('>', "&gt;")
-        .replace('"', "&quot;"));
+    writer.append(escapeHtml(token.content));
 };
 
 export = inline;
