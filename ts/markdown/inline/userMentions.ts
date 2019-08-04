dc8ed4224ac4a56998d7ba2b615bdbc8b9b751f0
anatawa12 <anatawa12@icloud.com>
2019-08-04 22:42:38 +0900

    add userMentions

---
+++ ts/markdown/inline/userMentions.ts
@@ -0,0 +1,16 @@
+import {InlineParser} from "../InlineParser";
+import {snowflakeToken} from "../Tokens";
+
+const userMentions: InlineParser = function userMentions(state, env): boolean {
+    const match = state.match(/^<@!?([0-9]+)>/);
+    if (match == null) return false;
+
+    state.goCursor(match.groups![0].length);
+
+    const id = match.groups![1];
+
+    state.pushToken(snowflakeToken("user", match.groups![0], id));
+    return true;
+};
+
+export = userMentions;
