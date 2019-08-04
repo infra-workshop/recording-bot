85b7947489a127c19879458e8d153ba162c5f76e
anatawa12 <anatawa12@icloud.com>
2019-08-04 22:42:04 +0900

    add DiscordSnowflakeToken

--- ts/markdown/Tokens.ts
+++ ts/markdown/Tokens.ts
@@ -1,4 +1,5 @@
 import {Token} from "./Token";
+import {Snowflake} from "discord.js";
 
 export interface InlineToken extends Token {
     readonly name: "inline";
@@ -12,5 +13,24 @@ export function inlineToken(content: string): InlineToken {
         name: "inline",
         indent: 0,
         content: content
+    }
+}
+
+export interface DiscordSnowflakeToken extends Token {
+    readonly name: string;
+    readonly tag?: undefined;
+    readonly indent: 0;
+    readonly content: string;
+    readonly snowflake: Snowflake;
+}
+
+export type SnowflakeKind = "global" | "user" | "role" | "channel" | "emoji";
+
+export function snowflakeToken(kind: SnowflakeKind, content: string, snowflake: Snowflake): DiscordSnowflakeToken {
+    return {
+        name: "snowflake_" + kind,
+        indent: 0,
+        content: content,
+        snowflake: snowflake
     }
 }
