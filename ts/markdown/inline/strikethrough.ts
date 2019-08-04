aac694a02854cb71f69f2644bc72f678fd2d9dde
anatawa12 <anatawa12@icloud.com>
2019-08-04 20:47:13 +0900

    add strikethrough

---
+++ ts/markdown/inline/strikethrough.ts
@@ -0,0 +1,30 @@
+import {InlineParser} from "../InlineParser";
+
+const strikethrough: InlineParser = function strikethrough(state, env): boolean {
+    if (state.get(2) !== "~~") return false;
+    const closeIndex = state.indexOf("~~", 3);
+    if (closeIndex == -1) return false;
+
+    state.goCursor(2);
+
+    state.pushToken({
+        name: "s_open",
+        tag: "s",
+        indent: 1
+    });
+
+    state.parseUntil(closeIndex - 2, env);
+
+
+    state.pushToken({
+        name: "s_close",
+        tag: "s",
+        indent: -1
+    });
+
+    state.goCursor(2);
+
+    return true;
+};
+
+export = strikethrough;
