a14d80e7bc2c6ffe8e6942fa61af3412d4c2a27a
anatawa12 <anatawa12@icloud.com>
2019-08-04 16:32:27 +0900

    setting for electron

---
+++ ts/electron.ts
@@ -0,0 +1,28 @@
+import {app, BrowserWindow} from 'electron';
+
+let mainWindow = null;
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', async function () {
+
+    // ブラウザ(Chromium)の起動, 初期画面のロード
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        frame: false
+    });
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+
+    await mainWindow.loadURL('file://' + __dirname + '/../html/electron.html');
+});
