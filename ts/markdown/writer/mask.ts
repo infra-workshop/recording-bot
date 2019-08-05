6285781645ff175b750102e82d15e748a0513e61
anatawa12 <anatawa12@icloud.com>
2019-08-05 09:35:14 +0900

    fix masks

--- ts/markdown/writer/mask.ts
+++ ts/markdown/writer/mask.ts
@@ -3,9 +3,9 @@ import {Writer} from "./Writer";
 import {Token} from "../Token";
 
 export const mask_open: CustomWriter<Token> = function mask_open(writer: Writer, token: Token) {
-    writer.append(`<span class="masked-text">`)
+    writer.append(`<span class="mask-span"><span class="masked-text">`)
 };
 
 export const mask_close: CustomWriter<Token> = function mask_close(writer: Writer, token: Token) {
-    writer.append(`</span>`)
+    writer.append(`</span></span>`)
 };
