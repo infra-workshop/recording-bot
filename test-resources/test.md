d59f8eaa0499b7013309bb2ae14ee6e773919e81
anatawa12 <anatawa12@icloud.com>
2019-08-05 10:23:54 +0900

    add test

---
+++ test-resources/test.md
@@ -0,0 +1,19 @@
+__1~~2**3 _4_ 5**6~~7__
+||mask||
+`inline-code`
+```
+block code 
+without first line
+```
+```block code 
+with first line
+```
+```kotlin
+fun markup_test() {
+    println("string" + 10)
+}
+```
+
+http://google.com
+http://google/
+plane text here
---
+++ ts/tests/markdown.ts
@@ -0,0 +1,15 @@
+import * as chai from 'chai';
+import * as fs from "fs";
+import {markdown} from "../markdown";
+
+describe('markdownのテスト', () => {
+
+    it('フルテスト', () => {
+
+        const resourcesPath = __dirname + "/../../../test-resources/";
+
+        const md = fs.readFileSync(resourcesPath + "test.md").toString("utf8");
+
+        chai.assert.equal(markdown(md), fs.readFileSync(resourcesPath + "test.md.html").toString("utf8"));
+    })
+});
