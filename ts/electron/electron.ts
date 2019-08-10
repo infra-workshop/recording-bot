f648031c7b765e6f9d72351abb3ad59b8f59f7ad
anatawa12 <anatawa12@icloud.com>
2019-08-10 11:04:30 +0900

    Revert "Merge branch 'remove-electron'"
    This reverts commit bf389a1330088a7b5b76922bf076cc1b09c54b4a, reversing
    changes made to 6f2fc947258b52f655f3f8a44a51152e6777c2fa.

---
+++ ts/electron/electron.ts
@@ -0,0 +1,31 @@
+import {app, BrowserWindow} from 'electron';
+
+let mainWindow = null;
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', async function () {
+
+    // ブラウザ(Chromium)の起動, 初期画面のロード
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: false,
+        //only for release
+        //show: false,
+        frame: false
+    });
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+
+    await mainWindow.loadURL('file://' + __dirname + '/../../html/electron.html');
+});
