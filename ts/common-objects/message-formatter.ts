a84e114e5a4ca052da090e6af8aeba08797fc2fe
anatawa12 <anatawa12@icloud.com>
2019-11-04 23:40:56 +0900

    表示する機能を追加

--- ts/common-objects/message-formatter.ts
+++ ts/common-objects/message-formatter.ts
@@ -11,8 +11,41 @@ class MessageFormatter {
                 message: msg
             }
         };
-        return markdown(msg.content, options);
+        let result = markdown(msg.content, options);
+
+        if (msg.embeds.length != 0) {
+            // with embed
+            const append = "<span class='gray'>&lt;ambeds&gt;</span>";
+            result = result == "" ? append : `${result}\n${append}`;
+        }
+
+        if (msg.attachments.size != 0) {
+            let isFile = false;
+            let isImage = false;
+            for (let value of msg.attachments.values()) {
+                if (value.height) {
+                    isImage = true
+                } else {
+                    isFile = true
+                }
+                if (isImage && isFile) break
+            }
+            const fileTypes: string[] = [];
+            if (isFile) fileTypes.push("files");
+            if (isImage) fileTypes.push("images", "videos");
+
+            const append = `<span class='gray'>&lt;${makeAOrB(fileTypes)}&gt;</span>`;
+            result = result == "" ? append : `${result}\n${append}`;
+        }
+
+        return result;
     }
+}
+
+function makeAOrB(values: string[]): string {
+    if (values.length == 1) return values[0];
+    const rest = values.pop();
+    return values.join(", ") + ", or " + rest
 }
 
 export default new MessageFormatter();
